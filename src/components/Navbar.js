2ea655cc22fa62af0f51235217b3b81863d9f285
Tahasdq <64333343+Tahasdq@users.noreply.github.com>
2022-08-31 02:23:49 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,14 +21,14 @@ const Navbar = () => {
       <div>
 
 
-        <a  className='navlinks'   href="/miphones">MI PHONES</a>
-        <a  className='navlinks'  href="/redmiphones">REDMI PHONES</a>
-        <a  className='navlinks'  href="/tv">TV</a>
-        <a className='navlinks'   href="/laptops">LAPTOP</a>
-        <a  className='navlinks'  href="/lifestyle">FITNESS AND LIFESTYLE</a>
-        <a  className='navlinks'  href="/home">HOME</a>
-        <a  className='navlinks'  href="/audio">AUDIO</a>
-        <a  className='navlinks'  href="/accessories">ACCESSORIES</a>
+           <a  className="navlinks" href="/#miphones">Mi Phones</a>
+           <a   className="navlinks"  href="/#redmiphones">Redmi Phones</a>
+           <a   className="navlinks" href="/#tv">TV</a>
+           <a   className="navlinks" href="/#laptops">Laptops</a>
+           <a   className="navlinks" href="/#lifestyle">Fitness & Lifestyle</a>
+           <a  className="navlinks" href="/#home">Home</a>
+           <a   className="navlinks" href="/#audio">Radio</a>
+           <a   className="navlinks" href="/#accessories">Accessories</a>
       </div>
 
 

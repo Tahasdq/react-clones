e38c1f5f6d051030ecb2a069480a64a39674b20c
Tahasdq <64333343+Tahasdq@users.noreply.github.com>
2022-08-31 01:43:47 +0500

    2nd commit
    again made changes in carrousel

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../styles/Slider.css";
-import Carousel from 'react-bootstrap';
+import {Carousel} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Slider = ({start}) => {
